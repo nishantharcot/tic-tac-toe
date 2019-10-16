6941cb6b07b81cf58556a2623725586fd2a2aa38
Arcot Nishanth <nsharc4@gmail.com>
2019-10-16 17:23:25 +0530

    basic game functioning done

---
+++ mvc.js
@@ -0,0 +1,129 @@
+(
+  document.onreadystatechange = () => {
+    if (document.readyState === 'complete') {
+      const x = 'X';
+      const o = 'O';
+      let count = 0;
+      let score_player1 = 0;
+      let score_player2 = 0;
+      const boxes = document.getElementsByClassName('box');
+      // const scores = document.getElementById('Score');
+      let model = {
+        board: [
+          ['', '', ''],
+          ['', '', ''],
+          ['', '', ''],
+        ],
+        currentPlayer: x,
+      }
+      let boardView = {
+        render: () => {
+          for (let i = 0; i < boxes.length; i += 1) {
+            const row = Math.floor(i / 3);
+            const col = Number(i % 3);
+            boxes[i].innerHTML = model.board[row][col];
+          }
+        },
+        attachEventHandlers: () => {
+          for (let i = 0; i < boxes.length; i += 1) {
+            boxes[i].onclick = () => {
+              controller.handleBoxClick(i);
+            };
+          }
+        },
+        init: () => {
+          boardView.attachEventHandlers();
+        }
+      }
+      let controller = {
+        handleBoxClick(boxIndex) {
+          const row = Math.floor(boxIndex / 3);
+          const col = Number(boxIndex % 3);
+          if (model.board[row][col]) {
+            alert('Invalid Move');
+          } else {
+            model.board[row][col] = model.currentPlayer;
+            model.currentPlayer = model.currentPlayer === x ? o : x;
+            count += 1;
+            boardView.render();
+            if (count >= 5) {
+              controller.checkGame(model.board);
+            }
+            if (count === 9) {
+              controller.clearBoard(model.board);
+            }
+          }
+        },
+        checkGame(board) {
+          for (let row = 0; row < 3; row++) {
+            if (board[row][0] === board[row][1] & board[row][1] === board[row][2]) {
+              if (board[row][0] === 'X') {
+                score_player1 += 10;
+                alert('Player 1 wins');
+                controller.clearBoard(board);
+              }
+              if (board[row][0] === 'O') {
+                score_player2 += 10;
+                alert('Player 2 wins');
+                controller.clearBoard(board);
+              }
+            }
+          }
+          for (let col = 0; col < 3; col++) {
+            if (board[0][col] === board[1][col] & board[1][col] === board[2][col]) {
+              if (board[0][col] === 'X') {
+                score_player1 += 10;
+                alert('Player 1 wins');
+                controller.clearBoard(board);
+              }
+              if (board[0][col] === 'O') {
+                score_player2 += 10;
+                alert('Player 2 wins');
+                controller.clearBoard(board);
+              }
+            } 
+          }
+          if (board[0][0] === board[1][1] & board[1][1] === board[2][2]) {
+            if (board[0][0] === 'X') {
+                score_player1 += 10;
+                alert('Player 1 wins');
+                controller.clearBoard(board);
+              }
+            if (board[0][0] === 'O') {
+                score_player2 += 10;
+                alert('Player 2 wins');
+                controller.clearBoard(board);
+              }
+          }
+          if (board[0][2] === board[1][1] & board[1][1] === board[2][0]) {
+            if (board[0][2] === 'X') {
+                score_player1 += 10;
+                alert('Player 1 wins');
+                controller.clearBoard(board);
+              }
+            if (board[0][2] === 'O') {
+                score_player2 += 10;
+                alert('Player 2 wins');
+                controller.clearBoard(board);
+              }
+          }
+        },
+        clearBoard(board) {
+          for (let row = 0; row < 3; row++) {
+            for (let col =0; col < 3; col++) {
+              board[row][col] = '';
+            }
+          }
+          boardView.render();
+          model.currentPlayer = x;
+          count = 0;
+        },
+        init: () => {
+          boardView.init();
+          boardView.render();
+        }
+      }
+      controller.init();
+    }
+  }
+)();
